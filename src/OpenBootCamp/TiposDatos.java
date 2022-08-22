56a942a65f898beddd51e55c876118e43dd6b015
Hugo Romero <hurodev2@gmail.com>
2022-08-22 11:58:53 -0500

    Initial commit

---
+++ src/OpenBootCamp/TiposDatos.java
@@ -0,0 +1,22 @@
+package OpenBootCamp;
+
+public class TiposDatos {
+
+    public static void main(String[] args) {
+
+
+
+        String nombre  = "Hugo Romero";
+        int edad = 33;
+        long ids  = 12345678910L;
+        double id = 1.2345e300d;
+        boolean ImDeveloper = true;
+        float pi = 3.1416f;
+
+
+        System.out.println(nombre);
+    }
+
+
+
+}
